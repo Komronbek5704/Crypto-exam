ed3dee0ad6cf89dea9c47bd47bc487d2cbfbce90
Komronbek5704 <qudratovkomron2004@gmail.com>
2024-08-16 04:10:54 +0500

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-    const navigate = useNavigate();
+  
 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-[#16171A] text-white">
